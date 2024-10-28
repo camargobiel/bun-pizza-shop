2018e1a77c0a1bcd95a70ff0851d98f904b7ae5d
Gabriel Camargo <camargoobiel@gmail.com>
2024-10-28 13:27:47 -0300

    feat: 🎸 get month receipt route

---
+++ src/http/routes/get-month-receipt.ts
@@ -0,0 +1,55 @@
+import Elysia from 'elysia'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import dayjs from 'dayjs'
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { and, eq, gte, sql, sum } from 'drizzle-orm'
+
+export const getMonthReceipt = new Elysia()
+  .use(auth)
+  .get('/metrics/month-receipt', async ({ getCurrentUser }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const today = dayjs()
+    const startOfLastMonth = today.subtract(1, 'month').startOf('month')
+
+    const monthsReceipts = await db
+      .select({
+        monthWithYear: sql<string>`TO_CHAR(${orders.createdAt}, 'YYYY-MM')`,
+        receipt: sum(orders.totalInCents).mapWith(Number),
+      })
+      .from(orders)
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          gte(orders.createdAt, startOfLastMonth.toDate()),
+        ),
+      )
+      .groupBy(sql`TO_CHAR(${orders.createdAt}, 'YYYY-MM')`)
+
+    const currentMonthWithYear = today.format('YYYY-MM')
+    const lastMonthWithYear = startOfLastMonth.format('YYYY-MM')
+
+    const currentMonthReceipt = monthsReceipts.find(
+      (receipt) => receipt.monthWithYear === currentMonthWithYear,
+    )
+
+    const lastMonthReceipt = monthsReceipts.find(
+      (receipt) => receipt.monthWithYear === lastMonthWithYear,
+    )
+
+    const diffFromLastMonth =
+      currentMonthReceipt && lastMonthReceipt
+        ? (currentMonthReceipt.receipt * 100) / lastMonthReceipt.receipt
+        : 0
+
+    return {
+      receipt: currentMonthReceipt?.receipt || 0,
+      diffFromLastMonth: Number((diffFromLastMonth - 100).toFixed(2)),
+    }
+  })
